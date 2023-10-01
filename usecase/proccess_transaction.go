513e9c4970f8aded44bc366ea8e445925adbfdac
ruhancs <ruhan_cs@hotmail.com>
2023-10-01 19:14:21 -0300

    feat: :sparkles: adicionado proccessTransaction use case

---
+++ usecase/proccess_transaction.go
@@ -0,0 +1,53 @@
+package usecase
+
+import (
+	"time"
+
+	"github.com/ruhancs/card_transaction/domain/entity"
+	"github.com/ruhancs/card_transaction/domain/infra/gateway"
+	"github.com/ruhancs/card_transaction/dto"
+)
+
+type UseCaseTransaction struct {
+	TransactionRepository gateway.TransactionRepositoryInterface
+}
+
+func NewTransactionUseCase(repository gateway.TransactionRepositoryInterface) UseCaseTransaction {
+	return UseCaseTransaction{
+		TransactionRepository: repository,
+	}
+}
+
+func (usecase *UseCaseTransaction) ProccessTransaction(transactionDTO dto.Transaction) (entity.Transaction,error) {
+	creditCard := entity.NewCreditCard()
+	creditCard.Name = transactionDTO.Name
+	creditCard.Number = transactionDTO.Number
+	creditCard.ExpirationMonth = transactionDTO.ExpirationMonth
+	creditCard.ExpirationYear = transactionDTO.ExpirationYear
+	creditCard.CVV = transactionDTO.CVV
+
+	creditCardBalanceAndLimit,err := usecase.TransactionRepository.GetCreditCard(*creditCard)
+	if err != nil {
+		return entity.Transaction{},err
+	}
+	creditCard.ID = creditCardBalanceAndLimit.ID
+	creditCard.Limit = creditCardBalanceAndLimit.Limit
+	creditCard.Balance = creditCardBalanceAndLimit.Balance
+
+	t := entity.NewTransaction()
+	t.CreditCardID = creditCard.ID
+	t.Amount = transactionDTO.Amount
+	t.Status = transactionDTO.Store
+	t.Description = transactionDTO.Description
+	t.CreatedAt = time.Now()
+
+	//validacao do limit para transacao
+	t.ProccessAndValidate(creditCard)
+
+	err = usecase.TransactionRepository.SaveTransaction(*t,*creditCard)
+	if err != nil {
+		return entity.Transaction{},err
+	}
+
+	return *t,nil
+}
